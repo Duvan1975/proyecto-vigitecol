9c9a3fb8cae48b8acea222213dc9785b04711305
Duvan Ballesteros <ballexplorer1975@gmail.com>
2025-06-23 12:13:43 -0500

    Realizando pruebas en el formulario

--- frontend/src/components/Tabla.jsx
+++ frontend/src/components/Tabla.jsx
@@ -23,18 +23,18 @@ export function Tabla() {
             </thead>
             <tbody>
                 <tr>
-                    <td>Duván</td>
-                    <td>Ballesteros Gallego</td>
-                    <td>CC</td>
-                    <td>75086155</td>
-                    <td>10/09/1975</td>
-                    <td>Manizales</td>
-                    <td>Manizales</td>
-                    <td>PRIMERA</td>
-                    <td>CASADO</td>
+                    <td>José</td>
+                    <td>García</td>
+                    <td>CE</td>
+                    <td>11111125</td>
+                    <td>10/09/2003</td>
+                    <td>Montería</td>
+                    <td>Medellín</td>
+                    <td>SEGUNDA</td>
+                    <td>SOLTERO</td>
                     <td>MASCULINO</td>
-                    <td>Cra. 10 Norte 12a Occidente</td>
-                    <td>3218003734</td>
+                    <td>Av. 6a calle 15</td>
+                    <td>321525253</td>
                     <td>prueba1@hotmail.com</td>
                     <td>OPERATIVO</td>
                     <td>GUARDA DE SEGURIDAD</td>
