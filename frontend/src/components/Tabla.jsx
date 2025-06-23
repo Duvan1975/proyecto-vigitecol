863c96930fda2b53967703a9b4e67963a87a783e
Duvan Ballesteros <ballexplorer1975@gmail.com>
2025-06-23 12:08:06 -0500

    Preparando el formulario con todos los campos de la entidad Empleado en el frontend

---
+++ frontend/src/components/Tabla.jsx
@@ -0,0 +1,46 @@
+export function Tabla() {
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombres</th>
+                    <th>Apellidos</th>
+                    <th>Tipo Documento</th>
+                    <th>Número Documento</th>
+                    <th>Fecha Nacimiento</th>
+                    <th>Lugar Nacimiento</th>
+                    <th>Ciudad Expedición</th>
+                    <th>Libreta Militar</th>
+                    <th>Estado Civil</th>
+                    <th>Género</th>
+                    <th>Dirección</th>
+                    <th>Teléfono</th>
+                    <th>Correo</th>
+                    <th>Tipo Empleado</th>
+                    <th>Cargo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Duván</td>
+                    <td>Ballesteros Gallego</td>
+                    <td>CC</td>
+                    <td>75086155</td>
+                    <td>10/09/1975</td>
+                    <td>Manizales</td>
+                    <td>Manizales</td>
+                    <td>PRIMERA</td>
+                    <td>CASADO</td>
+                    <td>MASCULINO</td>
+                    <td>Cra. 10 Norte 12a Occidente</td>
+                    <td>3218003734</td>
+                    <td>prueba1@hotmail.com</td>
+                    <td>OPERATIVO</td>
+                    <td>GUARDA DE SEGURIDAD</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+    
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,157 @@
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { CuadrosTexto } from './components/CuadrosTexto';
+import './styles/styles.css';
+import { Tabla } from './components/Tabla';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<div className='container'>
-  <h1>Formulario de Registro de Empleados <strong>Vigitecol Ltda.</strong></h1>
-  <button className='btn btn-success'>Iniciar</button>
-  <CuadrosTexto 
-  tamanoinput="col-md-6"
-  titulolabel="Ejemplo Label:" 
-  tipoinput="number" 
-  nombreinput="num1" 
-  idinput="num1" 
-  placeholderinput="Ejemplo de Placeholder" 
-  />
+  <h1 className='alinearTexto'>Formulario de Registro de Empleados <strong>Vigitecol Ltda.</strong></h1>
+  <div className='row'>
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Nombres:"
+      tipoinput="text"
+      nombreinput="nombres"
+      idinput="nombres"
+      placeholderinput="Ingrese los nombres"
+    />
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Apellidos:"
+      tipoinput="text"
+      nombreinput="apellidos"
+      idinput="apellidos"
+      placeholderinput="Ingrese los apellidos"
+    />
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Tipo Documento:"
+      tipoinput="text"
+      nombreinput="tipoDocumento"
+      idinput="tipoDocumento"
+      placeholderinput="Seleccione el tipo de documentor"
+    />
+  </div>
+
+  <div className='row'>
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Número Documento:"
+      tipoinput="number"
+      nombreinput="numeroDocumento"
+      idinput="numeroDocumento"
+      placeholderinput="Ingrese el número de documento"
+    />
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Fecha de Nacimiento:"
+      tipoinput="date"
+      nombreinput="fechaNacimiento"
+      idinput="fechaNacimiento"
+      placeholderinput="Seleccione la fecha de nacimiento"
+    />
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Lugar de Nacimiento:"
+      tipoinput="text"
+      nombreinput="lugarNacimiento"
+      idinput="lugarNacimiento"
+      placeholderinput="Ingrese el lugar de nacimiento"
+    />
+  </div>
+
+  <div className='row'>
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Ciudad de expedición del documento:"
+      tipoinput="text"
+      nombreinput="ciudadExpedicion"
+      idinput="ciudadExpedicion"
+      placeholderinput="Ingrese la ciudad de expedición del documento"
+    />
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Libreta Militar:"
+      tipoinput="text"
+      nombreinput="libretaMilitar"
+      idinput="libretaMilitar"
+      placeholderinput="Seleccione su estado"
+    />
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Estado Civil:"
+      tipoinput="text"
+      nombreinput="estadoCivil"
+      idinput="estadoCivil"
+      placeholderinput="Seleccione su estado civil"
+    />
+  </div>
+
+  <div className='row'>
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Género:"
+      tipoinput="text"
+      nombreinput="genero"
+      idinput="genero"
+      placeholderinput="Seleccione el tipo de genero"
+    />
+  </div>
+
+  <h3 className='alinearTexto'>Datos de Contacto</h3>
+
+  <div className='row'>
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Dirección:"
+      tipoinput="text"
+      nombreinput="direccion"
+      idinput="direccion"
+      placeholderinput="Ingrese la dirección"
+    />
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Teléfono:"
+      tipoinput="number"
+      nombreinput="telefono"
+      idinput="telefono"
+      placeholderinput="Ingrese el número telefónico"
+    />
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Correo Electrónico:"
+      tipoinput="email"
+      nombreinput="correo"
+      idinput="correo"
+      placeholderinput="Ingrese el correo electróncio"
+    />
+  </div>
+  <br></br>
+  <h3 className='alinearTexto'>Asignar Cargo</h3>
+
+  <div className='row'>
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Tipo de Empleado:"
+      tipoinput="text"
+      nombreinput="tipoEmpleado"
+      idinput="tipoEmpleado"
+      placeholderinput="Seleccione el tipo de empleado"
+    />
+    <CuadrosTexto
+      tamanoinput="col-md-4"
+      titulolabel="Cargo:"
+      tipoinput="text"
+      nombreinput="cargo"
+      idinput="cargo"
+      placeholderinput="Ingrese el cargo a ocupar"
+    />
+  </div>
+  <br></br>
+  <button className='botonregistrar btn btn-success'>Registrar</button>
+  <br></br>
+  <Tabla />
 </div>
+
 );
