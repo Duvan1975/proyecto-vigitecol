4f639c94852465dd6cb7198859c51b0a9454e5ae
Duvan Ballesteros <ballexplorer1975@gmail.com>
2025-09-23 14:49:41 -0500

    Solucionando 404 Not Found

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './styles/styles.css';
-import { Menu } from './components/Menu';
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./styles/styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Menu />);
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
