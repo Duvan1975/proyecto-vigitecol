3de3ec59eef8572d1fe01cedfbb2afde7f088967
Duvan Ballesteros <ballexplorer1975@gmail.com>
2025-10-08 19:15:03 -0500

    Configurando IP para realizar conexión con segundo pc de prueba

--- proyectoVigitecolSpringBoot/src/main/java/proyectoVigitecolSpringBoot/infra/security/SecurityConfigurations.java
+++ proyectoVigitecolSpringBoot/src/main/java/proyectoVigitecolSpringBoot/infra/security/SecurityConfigurations.java
@@ -36,8 +36,8 @@ public class SecurityConfigurations {
                             "https://proyecto-vigitecol-dsa7-ocljwnv39-duvans-projects-4d9c2e8d.vercel.app",
                             "https://jodie-biobibliographic-tidally.ngrok-free.dev",
                             "http://192.168.20.142:8081",
-                            "http://192.168.1.50:8081",
-                            "http://192.168.1.50:3000"
+                            "http://192.168.1.6:8081",
+                            "http://192.168.1.6:3000"
                     ));
                     corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                     corsConfiguration.setAllowedHeaders(List.of("*"));
