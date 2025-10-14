fade758c1fd8186673e9fd963118ac50d603066a
Duvan Ballesteros <ballexplorer1975@gmail.com>
2025-10-14 03:42:15 -0500

    Login modificando 8080

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,7 +8,7 @@ export function Login({ onLoginSuccess }) {
 
     const handleLogin = async () => {
         try {
-        const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:8081";
+        const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
         const response = await fetch(`${backendUrl}/login`, {
                 method: "POST",
                 headers: {
